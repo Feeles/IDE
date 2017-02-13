8c7dd2545b88a7e949572965b16f2a5df90e65a3
teramotodaiki <i101323@gmail.com>
2017-02-13 16:32:16 +0900

    Refactoring

--- src/Cards/EnvCard.jsx
+++ src/Cards/EnvCard.jsx
@@ -1,11 +1,10 @@
-import React, { PureComponent, PropTypes } from 'react';
-import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
+import React, {PureComponent, PropTypes} from 'react';
+import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card';
 
-
-import { SourceFile } from '../File/';
-import { Tab } from '../ChromeTab/';
-import EnvItem from './EnvItem';
-import { commonRoot } from './commonStyles';
+import {SourceFile} from '../File/';
+import {Tab} from '../ChromeTab/';
+import {EnvItem} from '../EnvPane/';
+import {commonRoot} from './commonStyles';
 import EditFile from './EditFile';
 
 export default class EnvCard extends PureComponent {
@@ -17,12 +16,12 @@ export default class EnvCard extends PureComponent {
     localization: PropTypes.object.isRequired,
     findFile: PropTypes.func.isRequired,
     selectTab: PropTypes.func.isRequired,
-    addFile: PropTypes.func.isRequired,
+    addFile: PropTypes.func.isRequired
   };
 
   state = {
     env: this.props.getConfig('env'),
-    fileKey: '',
+    fileKey: ''
   };
 
   componentWillReceiveProps(nextProps) {
@@ -30,7 +29,9 @@ export default class EnvCard extends PureComponent {
       const envFile = this.props.findFile('.env');
       this.setState({
         env: this.props.getConfig('env'),
-        fileKey: envFile ? envFile.key : '',
+        fileKey: envFile
+          ? envFile.key
+          : ''
       });
     }
   }
@@ -39,56 +40,32 @@ export default class EnvCard extends PureComponent {
     const envFile = this.props.findFile('.env');
     if (!envFile) {
       const env = this.props.getConfig('env');
-      this.props.addFile(
-        new SourceFile({
-          type: 'application/json',
-          name: '.env',
-          text: JSON.stringify(env, null, '\t'),
-        })
-      );
+      this.props.addFile(new SourceFile({
+        type: 'application/json',
+        name: '.env',
+        text: JSON.stringify(env, null, '\t')
+      }));
     } else {
-      this.setState({
-        fileKey: envFile.key,
-      });
+      this.setState({fileKey: envFile.key});
     }
   }
 
   handleUpdateEnv = (change) => {
     const env = Object.assign({}, this.state.env, change);
-    this.props.setConfig('env', env)
-      .then((file) => file.json)
-      .then((env) => this.setState({ env }));
+    this.props.setConfig('env', env).then((file) => file.json).then((env) => this.setState({env}));
   };
 
   render() {
-    const {
-      localization,
-    } = this.props;
+    const {localization} = this.props;
 
     return (
       <Card style={commonRoot}>
-        <CardHeader showExpandableButton actAsExpander
-          title={localization.envCard.title}
-          subtitle={localization.envCard.subtitle}
-        />
-        <CardText expandable >
-        {Object.keys(this.state.env).map((key) => (
-          <EnvItem
-            key={key}
-            itemKey={key}
-            item={this.state.env[key]}
-            localization={localization}
-            updateEnv={this.handleUpdateEnv}
-          />
-        ))}
+        <CardHeader showExpandableButton actAsExpander title={localization.envCard.title} subtitle={localization.envCard.subtitle}/>
+        <CardText expandable>
+          {Object.keys(this.state.env).map((key) => (<EnvItem key={key} itemKey={key} item={this.state.env[key]} localization={localization} updateEnv={this.handleUpdateEnv}/>))}
         </CardText>
-        <CardActions expandable >
-          <EditFile
-            fileKey={this.state.fileKey}
-            findFile={this.props.findFile}
-            selectTab={this.props.selectTab}
-            localization={localization}
-          />
+        <CardActions expandable>
+          <EditFile fileKey={this.state.fileKey} findFile={this.props.findFile} selectTab={this.props.selectTab} localization={localization}/>
         </CardActions>
       </Card>
     );
---
+++ src/PalettePane/index.js
@@ -0,0 +1,1 @@
+export { default as LayeredStyle } from './LayeredStyle';
