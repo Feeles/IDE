7187667c06aa46c90616b9a2b349310fe73eb5b4
teramotodaiki <i101323@gmail.com>
2017-02-23 16:32:12 +0900

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import init from './jsx/init';
 import { default as Feeles } from './jsx/RootComponent';
 
-const h4p = (...args) =>
-  new Promise((resolve, reject) => {
-    document.readyState === 'complete' ?
-      resolve(init(...args)) :
-      addEventListener('load', () => {
-        return resolve(init(...args));
-      });
-  });
+
+const hasLoaded = new Promise((resolve, reject) => {
+  if (document.readyState === 'complete') {
+    resolve();
+  } else {
+    window.addEventListener('load', () => resolve());
+  }
+});
+
+const h4p = async (props) => {
+  await hasLoaded;
+  init(props);
+};
 
 h4p.init = init;
 h4p.Feeles = Feeles;
