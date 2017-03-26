ee808839ec5eff100b62c0df2090462eaa038c65
teramotodaiki <i101323@gmail.com>
2017-03-26 16:44:10 +0900

    speech recognition and text to speech
    まだ実装されているブラウザが少ないので、それぞれ fallback を用意

--- samples/ask/ask.js
+++ samples/ask/ask.js
@@ -1,18 +1,49 @@
-export default function ask(message) {
+const {SpeechRecognition} = feeles;
 
-  if (typeof message === 'string') {
-
-    return new Promise((resolve, reject) => {
-      alert(message);
+function textToSpeech(message) {
+  const utter = new SpeechSynthesisUtterance(message);
+  return new Promise((resolve, reject) => {
+    utter.onend = (event) => {
       resolve();
-    });
+    };
+    utter.onerror = (event) => {
+      reject(event.error);
+    };
+    speechSynthesis.speak(utter);
+  });
+}
 
+function speechRecognition() {
+  const recognition = new SpeechRecognition();
+  return new Promise((resolve, reject) => {
+    recognition.onresult = (event) => {
+      console.info(event);
+      resolve(event.results[0][0].transcript);
+    };
+    recognition.onerror = (event) => {
+      console.error(event);
+      if (event.error === 'no-speech') {
+        resolve(''); // 無言だった
+      } else {
+        reject(event.error);
+      }
+    };
+    recognition.start();
+  });
+}
+
+const request = 'speechSynthesis' in window
+  ? textToSpeech
+  : (message) => Promise.resolve(alert(message));
+
+const response = SpeechRecognition
+  ? speechRecognition
+  : () => Promise.resolve(prompt());
+
+export default function ask(message) {
+  if (typeof message === 'string') {
+    return request(message);
   } else {
-
-    return new Promise((resolve, reject) => {
-      resolve(prompt(''));
-    });
-
+    return response();
   }
-
 }
