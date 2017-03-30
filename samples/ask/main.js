3fdf353c8603059033e5fa763b5bfec2ebbf055e
teramotodaiki <i101323@gmail.com>
2017-03-30 15:11:47 +0900

    update sample

--- samples/ask/main.js
+++ samples/ask/main.js
@@ -3,20 +3,30 @@ import internet from 'internet';
 
 async function start() {
 
-  await ask(`今日お昼何食べた？`);
 
-  const item1 = await ask();
   /* \____ assets/sample.json ____/ */
-
-  internet(item1).flickr.face();
-  await ask(`いいなあ、おいしそう`);
-  await ask(`${item1}の美味しい食べ方知ってる？`);
-
-  const item2 = await ask();
-
-  internet(`${item1} 美味しい食べ方`).youtube.card();
-  await ask(`こうやって食べるんだって`);
 
 }
 
 start();
+
+
+
+// サンプルコード
+
+// async function start() {
+//
+//   await ask(`今日お昼何食べた？`);
+//
+//   const item1 = await ask();
+//
+//   internet(item1).flickr.face();
+//   await ask(`いいなあ、おいしそう`);
+//   await ask(`${item1}の美味しい食べ方知ってる？`);
+//
+//   const item2 = await ask();
+//
+//   internet(`${item1} 美味しい食べ方`).youtube.card();
+//   await ask(`こうやって食べるんだって`);
+//
+// }
