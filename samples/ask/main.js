839aec66eba55aec0b3882a63a989d0581b47357
teramotodaiki <i101323@gmail.com>
2017-03-30 12:30:09 +0900

    resize canvas

--- samples/ask/main.js
+++ samples/ask/main.js
@@ -1,4 +1,10 @@
 import ask from 'ask';
 
+// canvas size
+for (const canvas of Array.from(document.getElementsByTagName('canvas'))) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
 import talk1 from './talks/1';
 talk1(ask);
