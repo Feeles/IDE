5c9c341d94c9531fafcf16ce9395a81695cded3a
teramotodaiki <i101323@gmail.com>
2017-05-09 12:03:41 +0900

    set atk in game.js

--- samples/make-rpg/game.js
+++ samples/make-rpg/game.js
@@ -4,10 +4,11 @@ import 'hackforplay/core';
 function gameFunc() {
 	Hack.maps['map1'].load(); // map1 をロード
 
-	self.player = new Player(); // プレイヤー（騎士）をつくる
+	self.player = new Player(); // プレイヤーをつくる
 	player.mod(_kきし); // 見た目
 	player.locate(3, 5); // はじめの位置
 	player.hp = 3; // 体力
+	player.atk = 1; // こうげき力
 	player.onbecomedead = () => {
 		player.destroy(); // プレイヤーがたおれた => プレイヤーを消す
 		Hack.gameover(); // プレイヤーがたおれた => ゲームオーバー
