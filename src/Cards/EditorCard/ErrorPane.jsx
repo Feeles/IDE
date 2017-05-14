68bcfcc737c12d2ede20bb7de6b29eb97f9d5d81
teramotodaiki <i101323@gmail.com>
2017-05-14 11:16:49 +0900

    show error with animation

--- src/Cards/EditorCard/ErrorPane.jsx
+++ src/Cards/EditorCard/ErrorPane.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import FlatButton from 'material-ui/FlatButton';
 import AlertError from 'material-ui/svg-icons/alert/error';
 import { red50, red500 } from 'material-ui/styles/colors';
@@ -13,13 +14,14 @@ export default class ErrorPane extends PureComponent {
 
   render() {
     const { error, localization, onRestore } = this.props;
-    if (!error) {
-      return null;
-    }
 
     const styles = {
+      group: {
+        flex: 0,
+        backgroundColor: 'rgb(245, 245, 245)'
+      },
       root: {
-        flex: '0 1 120px',
+        height: 120,
         borderStyle: 'double',
         borderColor: red500,
         backgroundColor: red50,
@@ -50,19 +52,37 @@ export default class ErrorPane extends PureComponent {
     };
 
     return (
-      <div style={styles.root}>
-        <div style={styles.heading}>
-          <AlertError color={red500} style={styles.errorIcon} />
-          <span>{localization.editorCard.error}</span>
-          <div style={styles.blank} />
-          <FlatButton
-            label={localization.editorCard.restore}
-            style={styles.restore}
-            onTouchTap={onRestore}
-          />
-        </div>
-        <pre style={styles.message}>{error.message}</pre>
-      </div>
+      <CSSTransitionGroup
+        transitionName={{
+          enter: 'zoomInUp',
+          enterActive: 'animated',
+          leave: 'fadeOut',
+          leaveActive: 'animated'
+        }}
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={500}
+        style={styles.groups}
+      >
+        {error
+          ? <div key="error" style={styles.root}>
+              <div style={styles.heading}>
+                <AlertError
+                  key="alert"
+                  color={red500}
+                  style={styles.errorIcon}
+                />
+                <span>{localization.editorCard.error}</span>
+                <div style={styles.blank} />
+                <FlatButton
+                  label={localization.editorCard.restore}
+                  style={styles.restore}
+                  onTouchTap={onRestore}
+                />
+              </div>
+              <pre style={styles.message}>{error.message}</pre>
+            </div>
+          : null}
+      </CSSTransitionGroup>
     );
   }
 }
