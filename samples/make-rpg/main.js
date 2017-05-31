fe2175e658943981087e38f6c250ea2d1081a0d6
teramotodaiki <i101323@gmail.com>
2017-05-31 09:33:13 +0900

    indent

--- samples/make-rpg/main.js
+++ samples/make-rpg/main.js
@@ -3,19 +3,19 @@ import 'hackforplay/core';
 // ゲームをつくる
 import gameFunc from './game';
 game.onload = () => {
-  gameFunc();
-  // Hack.player がないとき self.player を代わりに入れる
-  if (self.player && !Hack.player) {
-    Hack.player = self.player;
-  }
+	gameFunc();
+	// Hack.player がないとき self.player を代わりに入れる
+	if (self.player && !Hack.player) {
+		Hack.player = self.player;
+	}
 };
 
 // マップをつくる
 import maps from './maps';
 Hack.onload = () => {
-  // Hack.maps を事前に作っておく
-  Hack.maps = Hack.maps || {};
-  maps();
+	// Hack.maps を事前に作っておく
+	Hack.maps = Hack.maps || {};
+	maps();
 };
 
 // ゲームスタート
