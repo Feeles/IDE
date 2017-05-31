0bf0a3a42f1614c2d98e9a0bf42d5ef8dd73f1a6
teramotodaiki <i101323@gmail.com>
2017-05-31 09:49:07 +0900

    backword compatibility

--- samples/make-rpg/main.js
+++ samples/make-rpg/main.js
@@ -1,9 +1,23 @@
 import 'hackforplay/core';
 
-// ゲームをつくる
 import gameFunc from './game';
+import maps from './maps';
+
+let gameOnLoad, hackOnLoad;
+
+if (gameFunc._bundled) {
+	// (後方互換性) hackforplay.xyz 移植バージョン
+	gameOnLoad = gameFunc.gameOnLoad;
+	hackOnLoad = gameFunc.hackOnLoad;
+} else {
+	// ふつうはこちら
+	gameOnLoad = gameFunc;
+	hackOnLoad = maps;
+}
+
+// ゲームをつくる
 game.onload = () => {
-	gameFunc();
+	gameOnLoad();
 	// Hack.player がないとき self.player を代わりに入れる
 	if (self.player && !Hack.player) {
 		Hack.player = self.player;
@@ -11,11 +25,10 @@ game.onload = () => {
 };
 
 // マップをつくる
-import maps from './maps';
 Hack.onload = () => {
 	// Hack.maps を事前に作っておく
 	Hack.maps = Hack.maps || {};
-	maps();
+	hackOnLoad();
 };
 
 // ゲームスタート
