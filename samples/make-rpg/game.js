a9c2da57abe1f586acc7807c4bc2a4ea794ad892
teramotodaiki <i101323@gmail.com>
2017-06-11 12:49:48 +0900

    sample

--- samples/make-rpg/game.js
+++ samples/make-rpg/game.js
@@ -5,7 +5,7 @@ function gameFunc() {
 	Hack.changeMap('map1'); // map1 をロード
 
 	self.player = new Player(); // プレイヤーをつくる
-	player.mod(_kきし); // 見た目
+	player.mod( /*▼ スキン*/ (_kきし)); // 見た目
 	player.locate(3, 5); // はじめの位置
 	player.hp = 3; // 体力
 	player.atk = 1; // こうげき力
