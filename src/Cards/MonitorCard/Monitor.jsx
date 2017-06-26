0c40a6758bc2e967a02cfd94e80722ba7a556b78
teramotodaiki <i101323@gmail.com>
2017-06-26 15:38:52 +0900

    implement setInterval and clearInterval

--- src/Cards/MonitorCard/Monitor.jsx
+++ src/Cards/MonitorCard/Monitor.jsx
@@ -115,6 +115,8 @@ export default class Monitor extends PureComponent {
     on('message.api.SpeechRecognition', this.handleSpeechRecognition);
     on('message.setTimeout', this.handleSetTimeout);
     on('message.clearTimeout', this.handleClearTimeout);
+    on('message.setInterval', this.handleSetInterval);
+    on('message.clearInterval', this.handleClearInterval);
   }
 
   componentDidUpdate(prevProps, prevStates) {
@@ -346,6 +348,15 @@ export default class Monitor extends PureComponent {
   };
   handleClearTimeout = ({ data }) => {
     clearInterval(this.setTimeoutId.get(data.value.timeoutId));
+  };
+
+  setIntervalId = new Map();
+  handleSetInterval = ({ data, reply }) => {
+    const { intervalId, delay } = data.value;
+    this.setIntervalId.set(intervalId, setInterval(reply, delay));
+  };
+  handleClearInterval = ({ data }) => {
+    clearInterval(this.setIntervalId.get(data.value.intervalId));
   };
 
   handleSpeechRecognition = ({ data }) => {
