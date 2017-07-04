0c57332d473694b9d9ccc77f34d039be21f32fc1
teramotodaiki <i101323@gmail.com>
2017-07-04 10:21:32 +0900

    synonyms.synonyms

--- samples/mc/minecraft/synonyms.js
+++ samples/mc/minecraft/synonyms.js
@@ -5,5 +5,32 @@ export const events = {
 	died: 'やられた',
 
 	travelled: 'うごいた'
-
 };
+
+export const synonyms = {};
+
+const tryParse = async loc => {
+	try {
+		const result = await feeles.fetch(`feeles/blocks_${loc}.dropdown.yml`);
+		const yaml = await result.text();
+		return yaml.split('\n')
+			// s ===`  - body: "'air'"`
+			.map(s => s.substring(s.indexOf("'") + 1, s.lastIndexOf("'")));
+	} catch (e) {
+		return [];
+	}
+};
+
+(async () => {
+
+	const en = await tryParse('en');
+	const ja = await tryParse('ja');
+
+	for (const [i, value] of en.entries()) {
+		if (value) {
+			const synonym = ja[i];
+			synonyms[synonym] = value;
+		}
+	}
+
+})();
