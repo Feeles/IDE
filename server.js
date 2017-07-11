cc63626f5c0bb574673d3390578f59d1898bcfda
teramotodaiki <i101323@gmail.com>
2017-07-11 22:14:21 +0900

    enable CORS

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const version = require('./version');
 const server = http.createServer(async (request, response) => {
   const { pathname } = url.parse(request.url);
   response.writeHead(303, {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
     Location: await version.currentUrl(pathname)
   });
   response.end();
