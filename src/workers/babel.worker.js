3ddeb68cecd48c82ab3e292c189407a5b59261fc
teramotodaiki <i101323@gmail.com>
2017-12-26 13:50:05 +0900

    make inline worker in babel.worker.js

--- src/workers/babel.worker.js
+++ src/workers/babel.worker.js
@@ -1,22 +1,32 @@
-import { transform } from 'babel-standalone/babel.min';
+// raw-loader!escape-loader
+import babelStandaloneJs from 'raw-loader!../../lib/escape-loader!babel-standalone/babel.min';
 
-self.addEventListener('message', event => {
-  let code;
+const prefix = `
+self.addEventListener("message", function (event) {
   try {
     // Transpile the code
-    code = transform(event.data.code, event.data.options).code;
+    var code = Babel.transform(event.data.code, event.data.options).code;
     // Send result
     self.postMessage({
       id: event.data.id,
-      code
+      code: code
     });
   } catch (error) {
     // Throw babel error with processing
     self.postMessage({
       id: event.data.id,
-      error: { ...error, message: error.message }
+      error: { message: error.message, loc: error.loc }
     });
     // Throw useful error info into console
     throw error;
   }
 });
+`;
+
+const url = URL.createObjectURL(
+  new Blob([prefix, babelStandaloneJs], { type: 'text/javascript' })
+);
+
+export default function BabelWorker() {
+  return new Worker(url);
+}
