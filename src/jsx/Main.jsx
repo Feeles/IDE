15995309c5a5f4cf2901bbe0310c1cc1ecc4b00f
teramotodaiki <i101323@gmail.com>
2018-02-12 09:58:26 +0900

    styles: sizes

--- src/jsx/Main.jsx
+++ src/jsx/Main.jsx
@@ -52,8 +52,8 @@ const getStyle = (props, state, context) => {
 
     root: {
       position: 'relative',
-      width: '100vw',
-      height: '100vh',
+      width: '100%',
+      height: '100%',
       display: 'flex',
       flexDirection: 'column',
       backgroundColor: palette.backgroundColor
