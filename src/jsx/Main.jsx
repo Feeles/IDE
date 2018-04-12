94354cb8f8eaff268728b87feea6474c2d7852c2
teramotodaiki <i101323@gmail.com>
2018-04-12 13:51:03 +0900

    feature: update to material-ui@0.19.4 and remove react-tap-event-plugin

--- src/jsx/Main.jsx
+++ src/jsx/Main.jsx
@@ -22,12 +22,6 @@ const tryParseJSON = (text, defaultValue = {}) => {
   }
 };
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 import { FileView, createProject, updateProject } from '../database/';
 import { SourceFile, configs } from '../File/';
 import codemirrorStyle from '../js/codemirrorStyle';
