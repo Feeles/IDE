72bab65bc4f888b0c3429cde7711a64bc0c14a8a
teramotodaiki <i101323@gmail.com>
2018-05-12 20:45:08 +0900

    feature: babel-polyfill をデフォルトで追加

--- src/lib/screen.js
+++ src/lib/screen.js
@@ -3,9 +3,11 @@ var requireJs = require('raw-loader!./require.js');
 var connectorJs = require('raw-loader!./connector');
 var domtoimageJs = require('raw-loader!dom-to-image');
 var eventEmitter2Js = require('raw-loader!eventemitter2');
+var babelPolyfill = require('raw-loader!babel-polyfill/browser');
 
 module.exports = function(module) {
   return [
+    babelPolyfill,
     fetchPonyfill,
     eventEmitter2Js,
     module ? requireJs : '',
