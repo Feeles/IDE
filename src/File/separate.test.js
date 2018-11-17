e607b2e76c4a55e02403a8f87aca3d87bd7de6cf
teramotodaiki <i101323@gmail.com>
2018-11-17 23:51:58 +0900

    feature: add test

---
+++ src/File/separate.test.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+import separate from './separate';
+
+const cases = {
+  'root/sub/file.js': {
+    path: 'root/sub/',
+    plain: 'file',
+    ext: '.js',
+    name: 'root/sub/file.js',
+    moduleName: 'root/sub/file'
+  }
+};
+
+test('separate file path', t => {
+  for (const key of Object.keys(cases)) {
+    t.deepEqual(separate(key), cases[key]);
+  }
+});
